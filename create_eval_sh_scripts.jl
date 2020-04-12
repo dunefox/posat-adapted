1ead8f2a481c9bd76f5db40066f6db3a5dc97aab
dunefox <ph.fuchs@campus.lmu.de>
2020-04-12 17:59:30 +0200

    adapted julia scripts for slurm creation

--- create_eval_sh_scripts.jl
+++ create_eval_sh_scripts.jl
@@ -1,65 +1,30 @@
-function bias_set()
-  for train_weight=0.0:0.5:3.0, test_weight=0.0:0.5:3.0
-      open("results/bias_set_test_train$(train_weight)_test$(test_weight).sh", "w") do file
-          write(file, """#!/bin/sh
-          #SBATCH --gres=gpu:0
-          #SBATCH --partition=Luna,Gobi
-          #SBATCH --output=results/bias_set_test_train$(train_weight)_test$(test_weight).out
-          #SBATCH --job-name="bias_set_test_train$(train_weight)_test$(test_weight)"
+for train_weight=0.0:0.01:0.10
+    open("results/erw2_test_$(train_weight).sh", "w") do file
+        write(file, """#!/bin/sh
+              #SBATCH --gres=gpu:0
+              #SBATCH --partition=Luna,Gobi
+              #SBATCH --output=results/erw2_test_$(train_weight).out
+              #SBATCH --job-name="erw2_test_$(train_weight)"
 
-          python ../model/eval_bias.py saved_models/clean_ds_train --data_dir /big/f/fuchsp/data/datasets/train_tr --dataset test --bias_mode set --bias_set_train $(train_weight) --bias_fix_test -1.0 --bias_set_test $(test_weight) --model_desc bias_set_test_$(train_weight)_$(test_weight)
-          """)
-      end
-  end
+              python ../model/eval.py saved_models/erw2_train_$(train_weight) --data_dir /big/f/fuchsp/data/datasets/train_tr --eval_mode erw2 --dataset test --model_desc erw2_test_$(train_weight)
+
+              """)
+    end
 end
 
-function bias_fix()
-  for train_weight=0.0:0.5:3.0, test_weight=0.0:0.5:3.0
-      open("results/bias_fix_test_train$(train_weight)_test$(test_weight).sh", "w") do file
-          write(file, """#!/bin/sh
-          #SBATCH --gres=gpu:0
-          #SBATCH --partition=Luna,Gobi
-          #SBATCH --output=results/bias_fix_test_train$(train_weight)_test$(test_weight).out
-          #SBATCH --job-name="bias_fix_test_train$(train_weight)_test$(test_weight)"
+for train_weight=0.0:0.01:0.10
+    open("results/erw1_test_$(train_weight).sh", "w") do file
+        write(file, """#!/bin/sh
+              #SBATCH --gres=gpu:0
+              #SBATCH --partition=Luna,Gobi
+              #SBATCH --output=results/erw1_test_$(train_weight).out
+              #SBATCH --job-name="erw1_test_$(train_weight)"
 
-          python ../model/eval_bias.py saved_models/bias_fix_train_$(train_weight) --data_dir /big/f/fuchsp/data/datasets/train_tr --dataset test --bias_mode fix --bias_fix_test $(test_weight) --bias_set_test -1.0 --bias_set_train -1.0 --model_desc bias_fix_test_$(train_weight)_$(test_weight)
-          """)
-      end
-  end
+              python ../model/eval.py saved_models/erw1_train_$(train_weight) --data_dir /big/f/fuchsp/data/datasets/train_tr --eval_mode erw1 --dataset test --model_desc erw1_test_$(train_weight)
+
+              """)
+    end
 end
-
-function erw2()
-  for train_weight=0.0:0.01:0.08
-      open("results/erw2_test_$(train_weight).sh", "w") do file
-          write(file, """#!/bin/sh
-          #SBATCH --gres=gpu:0
-          #SBATCH --partition=Luna,Gobi
-          #SBATCH --output=results/erw2_test_$(train_weight).out
-          #SBATCH --job-name="erw2_test_$(train_weight)"
-
-          python ../model/eval.py saved_models/erw2_train_$(train_weight) --data_dir /big/f/fuchsp/data/datasets/train_tr --eval_mode erw2 --dataset test --model_desc erw2_test_$(train_weight)
-
-          """)
-      end
-  end
-end
-
-function erw1()
-  for train_weight=0.0:0.01:0.08
-      open("results/erw1_test_$(train_weight).sh", "w") do file
-          write(file, """#!/bin/sh
-          #SBATCH --gres=gpu:0
-          #SBATCH --partition=Luna,Gobi
-          #SBATCH --output=results/erw1_test_$(train_weight).out
-          #SBATCH --job-name="erw1_test_$(train_weight)"
-
-          python ../model/eval.py saved_models/erw1_train_$(train_weight) --data_dir /big/f/fuchsp/data/datasets/train_tr --eval_mode erw1 --dataset test --model_desc erw1_test_$(train_weight)
-
-          """)
-      end
-  end
-end
-
 
 open("results/finetune_attn_test.sh", "w") do file
   write(file, """#!/bin/sh
